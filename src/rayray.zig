39f017b59152fdb885bf6429ec39e861547bf720
Altagos <altagos@posteo.de>
2024-02-07 16:37:33 +0100

    raytracer struct

--- src/rayray.zig
+++ src/rayray.zig
@@ -1,10 +1,17 @@
 const std = @import("std");
-const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+pub const Raytracer = struct {
+    const Self = @This();
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
+    pub fn init() Self {
+        return .{};
+    }
+
+    pub fn deinit(self: *const Self) void {
+        _ = self;
+    }
+
+    pub fn render(self: *Self) void {
+        _ = self;
+    }
+};
